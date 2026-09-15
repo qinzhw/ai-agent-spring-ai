6c6f0751cc6e0e707434a4c3c44b01ec0540283b
qinzw <Hqcxj@1234>
2026-09-15 18:18:03 +0800

    应用 RAG 检索增强顾问

--- src/main/java/com/example/aiagent/app/LoveApp.java
+++ src/main/java/com/example/aiagent/app/LoveApp.java
@@ -9,6 +9,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
@@ -99,6 +100,9 @@ public class LoveApp {
     @Resource
     private VectorStore loveAppVectorStore;
 
+    @Resource
+    private Advisor loveAppRagCloudAdvisor;
+
     /**
      * 和 RAG 知识库进行对话
      * @param message
@@ -111,10 +115,16 @@ public class LoveApp {
                 .user(message)
                 .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+
                 // 开启日志，便于观察效果
                 .advisors(new MyLoggerAdvisor())
+
                 // 应用知识库问答
                 .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+
+                // 应用 RAG 检索增强顾问(基于阿里云知识库服务)
+//                .advisors(loveAppRagCloudAdvisor)
+
                 .call()
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
