c2e0700a5286c105fe296eb3f8c86ebcc0e63544
qinzhw <qinzhiwei200ok@163.com>
2026-09-18 18:10:37 +0800

    仿照OpenManus实现自定义的 MyManus 智能体

--- src/main/java/com/example/aiagent/tools/ToolRegistration.java
+++ src/main/java/com/example/aiagent/tools/ToolRegistration.java
@@ -25,6 +25,7 @@ public class ToolRegistration {
         ResourceDownloadTool resourceDownloadTool = new ResourceDownloadTool();
         TerminalOperationTool terminalOperationTool = new TerminalOperationTool();
         PDFGenerationTool pdfGenerationTool = new PDFGenerationTool();
+        TerminateTool terminateTool = new TerminateTool();
 
         // ToolCallbacks.from()：Spring AI 工具API，把工具实例包装成 ToolCallback 数组返回
         return ToolCallbacks.from(
@@ -33,7 +34,8 @@ public class ToolRegistration {
                 webScrapingTool,
                 resourceDownloadTool,
                 terminalOperationTool,
-                pdfGenerationTool
+                pdfGenerationTool,
+                terminateTool
         );
     }
 }
